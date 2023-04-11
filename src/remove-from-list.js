f0bf6592fee27f68870ced964cbbb8f340a3b63a
InventiveSpark <ReadingDude@gmail.com>
2023-04-12 02:57:57 +0500

    feat: add 'remove from list' solution

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -21,10 +21,48 @@ const { NotImplementedError } = require('../extensions/index.js');
  *     this.next = null;
  *   }
  * }
+ * 
+  Singly linked list l:
+    l - link to the head (first) node
+    |
+    v
+  ┌───┬───┐   ┌───┬───┐   ┌───┬───┐   ┌───┬───┐   ┌───┬───┐   ┌───┬───┐
+  | 3 |  ─┼─> | 1 |  ─┼─> | 2 |  ─┼─> | 3 |  ─┼─> | 4 |  ─┼─> | 5 | X │     X = NULL
+  └───┴───┘   └───┴───┘   └───┴───┘   └───┴───┘   └───┴───┘   └───┴───┘
+
+  Singly linked list after removeKFromList(l, 3):
+    l
+    |
+    v
+  ┌───┬───┐   ┌───┬───┐   ┌───┬───┐   ┌───┬───┐
+  | 1 |  ─┼─> | 2 |  ─┼─> | 4 |  ─┼─> | 5 | X │
+  └───┴───┘   └───┴───┘   └───┴───┘   └───┴───┘
+
+  Linked list element:
+      data    link to the next node
+        |       |
+        v       v
+    ┌───────┬──────┐
+    | value | next |
+    └───────┴──────┘
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  // Boundary case for stopping recursion:
+  if (!l) { // if list is empty
+    return null;
+  }
+
+  // If list is not empty
+  // calculate subsequent list from which all nodes with values equal to k are removed:
+  const subsequentList = removeKFromList(l.next, k); // recursive call
+
+  if (l.value === k) { // if the current node value is equal to k
+    return subsequentList; // remove (forget) the current node and return the subsequent list
+  }
+
+  // If the current node value is not equal to k:
+  l.next = subsequentList;  // link to the next node point to the subsequent list
+  return l; // return a list from which all nodes with values equal to k are removed
 }
 
 module.exports = {
