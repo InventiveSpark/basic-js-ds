702abb3ce6b7dab000c5cf99960fd888d14fa128
InventiveSpark <ReadingDude@gmail.com>
2023-04-11 17:59:13 +0500

    feat: add queue solution

--- extensions/list-node.js
+++ extensions/list-node.js
@@ -1,7 +1,16 @@
+/*
+  Linked list element:
+    data    link to the next node
+      |       |
+      v       v
+  ┌───────┬──────┐
+  | value | next |
+  └───────┴──────┘
+*/
 class ListNode {
   constructor(x) {
-    this.value = x;
-    this.next = null;
+    this.value = x; // data stored in the list element
+    this.next = null; // link to the next node or NULL value if the next node does not exist
   }
 }
 
--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -12,22 +12,58 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.enqueue(3); // adds the element to the queue
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
+
+  Queue follows the principle of FIFO (First In First Out):
+    front                   back
+      |                       |
+      v                       v
+    ┌───┬───┐   ┌───┬───┐   ┌───┬───┐
+    | 1 |  ─┼─> | 2 |  ─┼─> | 3 | X │             X = NULL
+    └───┴───┘   └───┴───┘   └───┴───┘
+      |                           ^
+      v                           |
+    dequeue (remove)            enqueue (add)
+
+  Linked list element:
+      data    link to the next node
+        |       |
+        v       v
+    ┌───────┬──────┐
+    | value | next |
+    └───────┴──────┘
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() { // create an empty queue
+    this.front = null; // link to the first node or NULL value if queue is empty
+    this.back = null;  // link to the last node or NULL value if queue is empty
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() { // return underlying linked list
+    return this.front;
   }
 
-  dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) { // enter queue, add an element at the end
+    const newNode = new ListNode(value); // create a new node
+    if (!this.front) { // if queue is empty
+      this.front = newNode;
+    }
+    else { // if queue is not empty
+      this.back.next = newNode;
+    }
+    this.back = newNode;
+  }
+
+  dequeue() { // leave queue, remove the front element and return it
+    if (!this.front) { // if queue is empty
+      return null;
+    }
+    // if queue is not empty:
+    if (this.front === this.back) { // if queue consists of one node
+      this.back = null;
+    }
+    const value = this.front.value;
+    this.front = this.front.next;
+    return value;
   }
 }
 
